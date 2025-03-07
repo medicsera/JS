8b15d7933e105afd0a953e23ae998dcef703c4a2
medicsera <anton.mirko.0550@gmail.com>
2025-03-07 16:23:15 +0700

    first task complete

--- YaP/4 лаба/main.js
+++ YaP/4 лаба/main.js
@@ -1,5 +1,4 @@
 //много текста
-
 function one(){
     function ask_password(login,password,success,failure){
         const vowels = (string) =>{
@@ -7,46 +6,43 @@ function one(){
             return matches;
         }
         const consonants = (string) => {
-            const matches = string.match(/[bcdfghjklmnpqrstvwxyz]/gi).toLowerCase();
+            const matches = string.match(/[bcdfghjklmnpqrstvwxyz]/gi);
             return matches;
         }
-        let error = ""
-        console.log(consonants(login))
-        console.log(consonants(password))
-        if (vowels(password).length !== 3){
-            error = "Wrong number of vowels"
+        if (vowels(password).length === 3 && consonants(password).length === 3 && consonants(login).toString() === consonants(password).toString()) {
+            success(login);
         }
-        if (vowels(password).length === 3 && consonants(password).length === 3 ){
-            if (consonants(login).toString() === consonants(password).toString()){
-                success(login)
-            }
-            else {
+        else {
+            let error = ""
+            if((vowels(password).length !== 3) && (consonants(login).toString() !== consonants(password).toString())){
                 error = "Everything is wrong"
             }
+            else if (vowels(password).length !== 3){
+                error = "Wrong number of vowels"
+            }
+            else if (consonants(login).toString() !== consonants(password).toString()) {
+                error = "Wrong consonants"
+            }
+            failure(login,error)
         }
-        else{
-            error = "Wrong consonants"
-        }
-
-    }
-    function onSuccess(login){
-        console.log(login)
-    }
-    function onError(error){
-        console.error(error)
     }
     function main(login,password){
-        this.login = login;
-        this.password = password;
-        const result = ask_password(login, password, onSuccess, onError)
-        if(result.error){
-            console.log(`Привет, ${login}!`)
-        }
-        else{
-            console.log(`Кто-то пытался притвориться пользователем ${login}, но в пароле допустил ошибку: ${onError()}`)
-        }
+        ask_password(
+            login,
+            password,
+            function (login){
+                console.log(`Привет, ${login}!`);
+            },
+            function (login, error) {
+                console.log(`Кто-то пытался притвориться пользователем ${login}, но в пароле допустил ошибку: ${error.toUpperCase()}`)
+            }
+        )
     }
     main("logni","aaalgn")
-
+    main("login","luagon")
+    main("login","aalgn")
+    main("login","aaagln")
+    main("login","fsfaa")
 }
 one()
+
